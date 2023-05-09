d0f3655ddfcf517122f1a9b679bb5b79110ffa39
alexi <alexi@MSI>
2023-05-09 18:56:51 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double num1, num2;
+
+Console.Write("Ingrese el primer numero: ");
+num1=double.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo numero: ");
+num2 = double.Parse(Console.ReadLine());
+
+if (num1 == num2)
+{
+    Console.WriteLine("Ambos numeros son iguales ");
+}
+else if (num1 > num2)
+{
+    Console.WriteLine($"{num1} es mayor a {num2}");
+}
+else 
+{
+    Console.WriteLine($"{num2} es mayor a {num1}");
+}
